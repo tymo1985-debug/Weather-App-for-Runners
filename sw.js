b334ea0a5e3bff9e1bd7dd9c097d7a932cfd8259
tymo1985-debug <tymo1985@googlemail.com>
2026-09-24 10:18:59 +0200

    Bump app shell cache for home redesign

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const PREFIX = 'weather-runner-';
-const SHELL_CACHE = `${PREFIX}shell-v31`;
+const SHELL_CACHE = `${PREFIX}shell-v32`;
 const API_CACHE = `${PREFIX}api-v6`;
 const API_TTL_MS = 5 * 60 * 1000;
 const API_MAX_ENTRIES = 20;
