ef759050ccf915439d6fdbe0925bd52494b3e5b6
tymo1985-debug <tymo1985@googlemail.com>
2026-09-24 19:51:39 +0200

    Refresh shell cache for 0.34.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const PREFIX = 'weather-runner-';
-const SHELL_CACHE = `${PREFIX}shell-v38`;
+const SHELL_CACHE = `${PREFIX}shell-v39`;
 const API_CACHE = `${PREFIX}api-v6`;
 const API_TTL_MS = 5 * 60 * 1000;
 const API_MAX_ENTRIES = 20;
